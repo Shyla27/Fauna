005762cbb3e7d7660364bb70f2cec02d04e448b2
Parrot Live user <user@localhost.localdomain>
2019-10-22 14:00:28 +0000

    main methods in animal class created

---
+++ src/main/java/Animals.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Animals {
+    private String name;
+    private String health;
+    private String age;
+    private Int id;
+
+
+    public Int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getHealth() {
+        return health;
+    }
+     public String getAge() {
+         return age;
+     }
+}
