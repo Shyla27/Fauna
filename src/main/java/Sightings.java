dad46642a2e288f6eee86220a41dcfa652f478f8
Parrot Live user <user@localhost.localdomain>
2019-10-22 14:01:35 +0000

    main methods in sighting class created

---
+++ src/main/java/Sightings.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sightings {
+     private String location;
+     private Int id;
+     private String name;
+
+
+     public getLocation() {
+         return location;
+     }
+      public getId() {
+          return id;
+      }
+      public getName() {
+          return name;
+      }
+      
+}
